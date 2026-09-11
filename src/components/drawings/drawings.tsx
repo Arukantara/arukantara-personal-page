056dea66c0b970d8ede6df29837d742455f496f4
José Alcántara Muñoz <alcantaramunozjose@gmail.com>
2026-09-11 21:43:23 +0200

    Refactoring CSS for drawings module

--- src/components/drawings/drawings.tsx
+++ src/components/drawings/drawings.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
+import styles from './drawings.module.scss';
 
 export default function Drawings() {
   return (
-    <div className="drawings">
-      <Image src="./sketches/mitsuru-w.png" width={200} height={200} alt="Dibujo de Kirijo Mitsuru" unoptimized className='fixed drawing-left'/>
-      <Image src="./sketches/link-w.png" width={200} height={200} alt="Dibujo de Link" unoptimized className='fixed drawing-center'/>
-      <Image src="./sketches/gatos-w.png" width={200} height={200} alt="Dibujo de Café y Mocha" unoptimized className='fixed drawing-right'/>
-      <Image src="./sketches/hoot-rat.png" width={100} height={100} alt="Hoot-rat" unoptimized className='fixed drawing-bottom'/>
+    <div className={styles.drawings}>
+      <Image src="./sketches/mitsuru-w.png" width={200} height={200} alt="Dibujo de Kirijo Mitsuru" unoptimized className={'fixed ' + styles['drawing-left']}/>
+      <Image src="./sketches/link-w.png" width={200} height={200} alt="Dibujo de Link" unoptimized className={'fixed ' + styles['drawing-center']}/>
+      <Image src="./sketches/gatos-w.png" width={200} height={200} alt="Dibujo de Café y Mocha" unoptimized className={'fixed ' + styles['drawing-right']}/>
+      <Image src="./sketches/hoot-rat.png" width={100} height={100} alt="Hoot-rat" unoptimized className={'fixed ' + styles['drawing-bottom']}/>
     </div>
   );
 }
